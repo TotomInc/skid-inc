012af83c5cae18e9acb0373f71544db3fe040217
Thomas <totom7700@gmail.com>
2016-06-10 14:14:32 +0000

    fixing some things

--- app/js/console/core.js
+++ app/js/console/core.js
@@ -46,45 +46,4 @@ game.console = {
         
         $('#console-input').val('');
     }
-    
-    // executer: function() {
-    //     var input = $('#console-input').val(),
-    //         cmd = input.split(' '),
-    //         thisCmd = game.console.cmds[cmd[0]],
-    //         execIndex = undefined,
-    //         argsExists = false;
-        
-    //     if (typeof game.console.cmds[cmd[0]] == 'object') {
-    //         for (var i = 0; i < thisCmd.args.length; i++) {
-    //             var argsCheck = [];
-                
-    //             for (var e = 0; e < thisCmd.args[i].length; e++) {
-    //                 if (cmd.length == thisCmd.args[i].length && cmd[e] == thisCmd.args[i][e] && argsCheck.length < cmd.length) {
-    //                     argsCheck.push(true);
-    //                     execIndex = i;
-
-    //                     if (argsCheck.length == cmd.length) {
-    //                         var thisCmdArgs = thisCmd.exec[i].substring(thisCmd.exec[i].indexOf('"') + 1, thisCmd.exec[i].length - 2);
-                            
-    //                         argsExists = true;
-    //                         eval(thisCmd.exec[execIndex]);
-    //                         game.console.latest = input;
-    //                         game.setInputTimeout();
-                            
-    //                         if (game.options.sounds)
-    //                             game.sounds.button.play();
-                            
-    //                         return;
-    //                     };
-    //                 };
-    //             };
-    //         };
-            
-    //         if (!argsExists)
-    //             game.console.print('error', game.console.errors.unknownArgs);
-    //     } else
-    //         game.console.print('error', game.console.errors.unknownCmd);
-        
-    //     $('#console-input').val('');
-    // }
 };
