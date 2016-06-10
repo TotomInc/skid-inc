e7b2139b3fe1048258e35d9da67c98cb75db2dc9
pullingour <pullingour@gmail.com>
2016-06-10 22:42:26 +0900

    add lz-string, compress save data

--- app/js/core/save.js
+++ app/js/core/save.js
@@ -1,28 +1,28 @@
 game.save = {
     key: 'SK-Inc',
-    
+
     save: function(from) {
-        localStorage.setItem(game.save.key, JSON.stringify(game.save.toSave));
-        
+      localStorage.setItem(game.save.key, LZString.compressToBase64(JSON.stringify(game.save.toSave)));
+
         if (from == "user")
             game.console.print('save', 'Game successfully saved to local-storage.');
         else
             console.log('Game saved.');
     },
-    
+
     reset: function() {
         clearInterval(game.options.intervals.save);
         localStorage.removeItem(game.save.key);
         location.reload();
     },
-    
+
     load: function() {
         if (localStorage.getItem(game.save.key) == null) {
             game.console.printGuide();
             console.warn('No save found!');
         }
         else {
-            var s = JSON.parse(localStorage.getItem(game.save.key)),
+          var s = JSON.parse(LZString.decompressFromBase64(localStorage.getItem(game.save.key)));
                 sgp = s.gp,
                 sga = s.ga,
                 sgo = s.go,
@@ -37,7 +37,7 @@ game.save = {
                 gs = game.servers,
                 gt = game.team,
                 gab = game.abilities;
-            
+
             if (go.version !== sgo.version)
                 console.warn('Loading savegame from an older version.');
 
@@ -52,7 +52,7 @@ game.save = {
 
             gp.timesHacked = sgp.timesHacked;
             gp.timesPlacesHacked = sgp.timesPlacesHacked;
-            
+
             gs.personal.owned = sgs.personal.owned;
             gs.personal.mult = sgs.personal.mult;
             gs.personal.level = sgs.personal.level;
@@ -61,14 +61,14 @@ game.save = {
             gs.professional.level = sgs.professional.level;
             gs.vm.owned = sgs.vm.owned;
             gs.quickhack.owned = sgs.quickhack.owned;
-            
+
             ga.owned = sga.owned;
             ga.printed = sga.printed;
 
             go.before = sgo.before;
             go.sounds = sgo.sounds;
             go.effectEnabled = sgo.effectEnabled;
-            
+
             gt.list['mini_market'].owned = sgt.list['mini_market'].owned;
             gt.list['market'].owned = sgt.list['market'].owned;
             gt.list['jewelry'].owned = sgt.list['jewelry'].owned;
@@ -76,7 +76,7 @@ game.save = {
             gt.list['trading_center'].owned = sgt.list['trading_center'].owned;
             gt.list['anonymous_hideout'].owned = sgt.list['anonymous_hideout'].owned;
             gt.list['deepweb'].owned = sgt.list['deepweb'].owned;
-            
+
             gt.list['mini_market'].progress = sgt.list['mini_market'].progress;
             gt.list['market'].progress = sgt.list['market'].progress;
             gt.list['jewelry'].progress = sgt.list['jewelry'].progress;
@@ -84,16 +84,16 @@ game.save = {
             gt.list['trading_center'].progress = sgt.list['trading_center'].progress;
             gt.list['anonymous_hideout'].progress = sgt.list['anonymous_hideout'].progress;
             gt.list['deepweb'].progress = sgt.list['deepweb'].progress;
-            
+
             gab.list['up-key'].owned = sgab.list['up-key'].owned;
-            
+
             game.achievements.checkLoaded();
-            
+
             console.info('Game loaded.');
             game.console.print('save', 'Save-game successfully loaded.');
         }
     },
-    
+
     varInit: function() {
         game.save.toSave = {
             'gp': game.player,
