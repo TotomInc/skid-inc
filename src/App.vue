2efd5446404038c567caa14d2344c7e90c28c472
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-16 11:48:57 +0200

    feat(terminal-input): add terminal-input component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div id="app"></div>
+  <div id="app" class="w-screen h-screen bg-grey-darkest">
+    <div id="terminal-wrapper" class="w-full h-full">
+      <terminal-input/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-property-decorator';
+import { Vue, Component } from 'vue-property-decorator';
 
+import TerminalInput from './components/TerminalInput.vue';
+
+@Component({
+  components: {
+    TerminalInput,
+  },
+})
 export default class App extends Vue {}
 </script>
 
-<style>
+<style lang="postcss" scoped>
 @import 'assets/styles/tailwind.postcss';
 </style>
---
+++ src/components/TerminalInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="terminal-input" class="flex items-center absolute pin-b w-full h-12 px-4 py-2 font-mono">
+    <span class="text-white text-lg mr-2">user@rpi $</span>
+    <div class="flex-1 text-white text-lg outline-none" contenteditable="true"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class TerminalInput extends Vue {}
+</script>
