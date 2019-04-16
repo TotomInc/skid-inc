79c9acede6cd600c0e06d7ac0f4e66c7c30b92cb
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-16 08:58:31 +0200

    chore: remove boilerplate code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import Vue from 'vue';
-import App from './App.vue';
-import store from './store';
+import Vue from "vue";
 
-Vue.config.productionTip = false;
+import App from "./App.vue";
+import store from "./store";
 
 new Vue({
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,12 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
+  state: {},
 
-  },
-  mutations: {
+  mutations: {},
 
-  },
-  actions: {
-
-  },
+  actions: {}
 });
