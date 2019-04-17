fdc9e39e2e00bbc60db3b40b0c0d3d293efbe5af
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-17 09:31:44 +0200

    feat(terminal-input): retrieve typed command from contenteditable

--- src/components/TerminalInput.vue
+++ src/components/TerminalInput.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="terminal-input" class="flex items-center absolute pin-b w-full h-12 px-4 py-2 font-mono">
     <span class="text-white text-lg mr-2">user@rpi $</span>
-    <div class="flex-1 text-white text-lg outline-none" contenteditable="true"></div>
+
+    <div
+      class="flex-1 text-white text-lg outline-none"
+      contenteditable="true"
+      @keyup="onInputKeyup($event)"
+    />
   </div>
 </template>
 
@@ -9,5 +14,15 @@
 import { Vue, Component } from 'vue-property-decorator';
 
 @Component({})
-export default class TerminalInput extends Vue {}
+export default class TerminalInput extends Vue {
+  public inputContent: string = '';
+
+  public onInputKeyup(event: KeyboardEvent): void {
+    const target = event.target as HTMLDivElement;
+
+    if (target && typeof target.textContent === 'string') {
+      this.inputContent = target.textContent;
+    }
+  }
+}
 </script>
