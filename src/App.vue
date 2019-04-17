e976be2f38369571705cb13fae34717fcd35bec8
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-17 10:44:05 +0200

    feat(terminal-logs): add component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="w-screen h-screen bg-grey-darkest">
     <div id="terminal-wrapper" class="w-full h-full">
+      <terminal-logs/>
       <terminal-input/>
     </div>
   </div>
@@ -9,10 +10,12 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 
-import TerminalInput from './components/TerminalInput.vue';
+import TerminalLogs from '@/components/TerminalLogs.vue';
+import TerminalInput from '@/components/TerminalInput.vue';
 
 @Component({
   components: {
+    TerminalLogs,
     TerminalInput,
   },
 })
---
+++ src/components/TerminalLogs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    id="terminal-logs"
+    class="w-full sm:w-2/3 md:w-1/2 px-4 py-2 font-mono"
+    :style="{ 'height': logHeight }"
+  ></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class TerminalLogs extends Vue {
+  get logHeight(): string {
+    const terminalInput = document.getElementById('terminal-input') as HTMLDivElement;
+
+    return `calc(100% - ${terminalInput.offsetHeight}px)`;
+  }
+}
+</script>
