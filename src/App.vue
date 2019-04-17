e1f90d30962b00624a9def9b6ee34350d73e5f51
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-17 13:58:22 +0200

    feat(logo-wrapper): add logo in the center of the terminal

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app" class="w-screen h-screen bg-grey-darkest">
+  <div id="app" class="relative w-screen h-screen bg-grey-darkest">
+    <logo-wrapper/>
+
     <div id="terminal-wrapper" class="w-full h-full">
       <terminal-logs/>
       <terminal-input/>
@@ -10,11 +12,13 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 
+import LogoWrapper from '@/components/LogoWrapper.vue';
 import TerminalLogs from '@/components/TerminalLogs.vue';
 import TerminalInput from '@/components/TerminalInput.vue';
 
 @Component({
   components: {
+    LogoWrapper,
     TerminalLogs,
     TerminalInput,
   },
---
+++ src/components/LogoWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="logo-wrapper">
+    <img src="@/assets/logo/logo-dark.png" alt="SkidInc logo">
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class LogoWrapper extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+#logo-wrapper {
+  pointer-events: none;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+
+  img {
+    display: block;
+    opacity: 0.1;
+  }
+}
+</style>
