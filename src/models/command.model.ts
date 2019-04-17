a4986ab59d4087c72e7ca52ef6d4a5b1d92b5a64
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-17 13:41:49 +0200

    feat(models): add custom command model

---
+++ src/models/command.model.ts
@@ -0,0 +1,10 @@
+import { Command as ParserCommand } from '@totominc/command-parser';
+
+export interface Command extends ParserCommand {
+  /** Condition to unlock the command */
+  unlock?: () => boolean;
+  /** Vuex action to emit on user-command success (complete) */
+  successAction: string;
+  /** Vuex action to emit on user-command failed (invalid) */
+  failedAction: string;
+}
