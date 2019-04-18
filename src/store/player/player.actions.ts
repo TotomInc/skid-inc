3963a36a9fe2908e5cf53ae35ba8610b82a18330
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-18 14:53:01 +0200

    feat(store): add actions and mutations for player module

--- src/store/player/player.actions.ts
+++ src/store/player/player.actions.ts
@@ -1,10 +1,43 @@
 import { ActionTree } from 'vuex';
+import plur from 'plur';
 
 import { PlayerState } from './player.state';
 import { RootState } from '../state';
+import { playerMutations } from './player.mutations';
+import { commandMutations } from '../command/command.mutations';
 
-export const playerActions = {};
+export const playerActions = {
+  LEVELUP: 'levelup',
+  EARN_EXPERIENCE: 'earnExperience',
+};
 
-const actions: ActionTree<PlayerState, RootState> = {};
+const actions: ActionTree<PlayerState, RootState> = {
+  [playerActions.LEVELUP](context) {
+    let levels = 0;
+
+    while (context.state.player.experience >= context.state.player.experienceRequired) {
+      context.commit(playerMutations.addLevel);
+      context.commit(playerMutations.setExperienceRequired);
+      context.commit(playerMutations.removeExperience, context.state.player.experienceRequired);
+
+      levels += 1;
+    }
+
+    if (levels) {
+      const log = `You gained ${levels} ${plur('level', levels)}!`;
+
+      context.dispatch(commandMutations.addLog, log);
+    }
+  },
+
+  /**
+   * An action is required to earn experience, since it needs to check for a
+   * potential levelup after mutating the amount of experience.
+   */
+  [playerActions.EARN_EXPERIENCE](context, payload: number) {
+    context.commit(playerMutations.addExperience, payload);
+    context.dispatch(playerActions.LEVELUP);
+  },
+};
 
 export default actions;
--- src/store/player/player.mutations.ts
+++ src/store/player/player.mutations.ts
@@ -2,8 +2,48 @@ import { MutationTree } from 'vuex';
 
 import { PlayerState } from './player.state';
 
-export const playerMutations = {};
+export const playerMutations = {
+  editUsername: 'editUsername',
 
-const mutations: MutationTree<PlayerState> = {};
+  addLevel: 'addLevel',
+  setExperienceRequired: 'setExperienceRequired',
+
+  addMoney: 'addMoney',
+  removeMoney: 'removeMoney',
+
+  addExperience: 'addExperience',
+  removeExperience: 'removeExperience',
+};
+
+const mutations: MutationTree<PlayerState> = {
+  [playerMutations.editUsername](state, payload: string) {
+    state.player.username = payload;
+  },
+
+  [playerMutations.addLevel](state) {
+    state.player.experience -= state.player.experienceRequired;
+    state.player.level += 1;
+  },
+
+  [playerMutations.setExperienceRequired](state) {
+    state.player.experienceRequired = Math.floor(100 * Math.pow(1.5, state.player.level + 1));
+  },
+
+  [playerMutations.addMoney](state, payload: number) {
+    state.player.money += payload;
+  },
+
+  [playerMutations.removeMoney](state, payload: number) {
+    state.player.money -= payload;
+  },
+
+  [playerMutations.addExperience](state, payload: number) {
+    state.player.experience += payload;
+  },
+
+  [playerMutations.removeExperience](state, payload: number) {
+    state.player.experience -= payload;
+  },
+};
 
 export default mutations;
