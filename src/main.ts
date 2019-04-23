8eb83940d4f59a64a8cff7bff5555a03f2bcbb99
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-23 16:56:05 +0200

    fix(main): export default vue instance

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
-new Vue({
+const vm = new Vue({
   store,
   render: (h) => h(App),
 }).$mount('#app');
+
+export default vm;
